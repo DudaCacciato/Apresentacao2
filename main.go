1cba3b1eb0493fcccdcaab7157e66d518c23a19e
Maria Cacciatore <maria.cacciatore@picpay.com>
2025-05-09 15:25:30 -0300

    Iniciando o código e criando o básico que seráusado

--- main.go
+++ main.go
@@ -1,6 +1,26 @@
 package main
 
+import (
+	"encoding/json"
+)
+
+//Interface 
+type atualizarEntrega interface {
+	AtualizarStatus() 
+}
+
+//Struct da entrega
+type entrega struct {
+	Entregador string `json:"entregador"`
+	Produto   string `json:"produto"`
+	Quantidade int    `json:"quantidade"`
+	Status []string `json:"status"`
+}
+
+//Método para atualizar o status da entrega
+
 func main() {
 
-	//Apenas testando o git
+	//lista de estatus 
+	status := []string{"pendente", "em andamento", "entregue"}
 }
